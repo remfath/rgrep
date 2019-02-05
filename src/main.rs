4daeaf22799e8ec294dfe32728520332dc06d883
Remfath Chan <remfath@gmail.com>
2019-02-05 23:15:11 +0800

    refactor struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for '{}' in file: {}...\n", query, filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("ERROR: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(filename).expect("read file error");
-    println!("\n============================");
+    if let Err(e) = run(config) {
+        println!("ERROR: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("\n========================================");
     println!("{}", contents);
-    println!("============================\n");
+    println!("========================================\n");
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("arguments length less than three!");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
