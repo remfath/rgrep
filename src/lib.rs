cddf7ab4be0f9a15a7bcb4b06e2559ea75d965e3
Remfath Chan <remfath@gmail.com>
2019-02-07 16:08:45 +0800

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,24 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("\n========================================");
-    println!("{}", contents);
-    println!("========================================\n");
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub struct Config {
@@ -22,5 +36,20 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
         Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 }
